91dde2c8eaa0e6b502c068bbd34c079e4bf249cd
Katagiri <katagiri@plala.co.jp>
2015-09-11 17:19:35 +0900

    ほぼ静的ページ作成

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -39,4 +42,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  config.include Rails.application.routes.url_helpers
+  config.include Capybara::DSL
+  
+
 end
