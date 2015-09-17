e74eccaeb63207e1e4efa52e529ff4598eb360ff
Katagiri <katagiri@plala.co.jp>
2015-09-17 16:18:46 +0900

    ユーザのつぶやき機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
     end
     
     def new
@@ -57,10 +58,7 @@ class UsersController < ApplicationController
     
     # Before actions
     
-    def signed_in_user
-        store_location
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
+
     
     def correct_user
         @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :microposts, dependent: :destroy
+
     before_save { self.email = email.downcase }
     before_create :create_remember_token
     
@@ -9,7 +11,11 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :password, length: { minimum: 6 }
     
-    
+    def feed
+        # このコードは準備段階です。
+        # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
+        Micropost.where("user_id = ?", id)
+    end
     
     def User.new_remember_token
         SecureRandom.urlsafe_base64
